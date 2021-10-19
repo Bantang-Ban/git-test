3c49b299d760b941d3a118604a850df5969547e6
xiaolingxi <1728957602@qq.com>
2021-10-19 16:18:05 +0800

    third commit

--- src/main/java/com/atguigu/git/GtiTest.java
+++ src/main/java/com/atguigu/git/GtiTest.java
@@ -4,5 +4,6 @@ public class GtiTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello gitee");
+        System.out.println("hello github");
     }
 }
