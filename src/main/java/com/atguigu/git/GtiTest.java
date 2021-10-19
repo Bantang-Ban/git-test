e5d96873f28a9329c4162bddb9da9fa87f8318a6
xiaolingxi <1728957602@qq.com>
2021-10-19 16:28:00 +0800

    fourth commit

--- src/main/java/com/atguigu/git/GtiTest.java
+++ src/main/java/com/atguigu/git/GtiTest.java
@@ -5,6 +5,5 @@ public class GtiTest {
         System.out.println("hello git");
         System.out.println("hello gitee");
         System.out.println("hello github");
-        System.out.println("hello coding");
     }
 }
